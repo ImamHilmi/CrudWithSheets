9dbfff8776339a190ff134ae1f776e3f80492af3
ImamHilmi <imam.hilmi253@gmail.com>
2022-06-20 15:12:00 +0700

    Init

---
+++ login.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Login Page</title>
+</head>
+<body>
+    <div class="formContainer">
+        <h2 class="title">
+            Login
+        </h2>
+        <div class="tip">
+            Login to your account using email and password
+        </div>
+        <label for="email">
+            Email
+        </label>
+    </div>
+</body>
+</html>
